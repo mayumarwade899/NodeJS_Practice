75eeb42e1af58c059f8efe1e38c111cd94c6493e
Mayur Marwade <mayumarwade899@gmail.com>
2024-10-11 19:45:32 +0530

    Building REST APIs added

---
+++ Project-1/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const PORT = 8000;
+
+//Routes
+app.get("/users", (req, res) => {
+  const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+  res.send(html);
+});
+
+//REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //Edit user with id
+    return res.join({ status: "pending" });
+  })
+  .delete((req, res) => {
+    //Delete user with id
+    return res.join({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //TODO: Create new user
+  return res.join({ status: "pending" });
+});
+
+app.listen(PORT, () => console.log(`Server started at Port ${PORT}`));
--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,14 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url")
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
+const app = express();
 
-  const log = `${Date.now()}: ${req.method} New req Received\n`;
-  const myUrl = url.parse(req.url, true);
-
-  fs.appendFile("log.txt", log, (err, result) => {
-    switch (myUrl.pathname) {
-      case "/":
-        if(req.method === "GET") res.end("Homepage")
-        break;
-
-      case "/about":
-        const username = myUrl.query.myname;
-        res.end(`Hi ${username}`);
-        break;
-
-      case "/search":
-        const search = myUrl.query.search_query;
-        res.end("Here are your result for " + search)
-        break;
-
-      case "/signup":
-        if(req.method === "GET") res.end("This is a sign up form");
-        else if(req.method === "POST") {
-          //DB Query
-          res.end("Success")
-        }
-
-      default:
-        res.end("404 Not Found");
-    }
-  });
+app.get("/", (req, res) => {
+  return res.send("Hello from HomePage");
 });
 
-myServer.listen(8000, () => console.log("Server Started..."));
+app.get("/about", (req, res) => {
+  return res.send(`Hello ${req.query.name}, your are ${req.query.age} years old`)
+})
+
+app.listen(8000, ()=> console.log("Server Started..."))
